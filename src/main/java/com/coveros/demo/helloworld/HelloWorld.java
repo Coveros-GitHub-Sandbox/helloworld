a48555a9391e84c1563e67b18a186b88f4bcf066
shabarishetty <shabarishetty46@gmail.com>
2019-08-27 14:36:02 +0530

    Update HelloWorld.java

--- src/main/java/com/coveros/demo/helloworld/HelloWorld.java
+++ src/main/java/com/coveros/demo/helloworld/HelloWorld.java
@@ -3,7 +3,7 @@ package com.coveros.demo.helloworld;
 public class HelloWorld {
 
   public static void main(final String[] args) {
-    System.out.println("Hello, World!");
+    System.out.println("Hello, shetty");
   }
 
 }
