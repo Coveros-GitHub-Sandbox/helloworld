0985481022a02b1f97b85f65dd6f439497169b16
dani <xpert_551@hotmail.com>
2019-10-21 21:30:31 +0100

    alteraçao

--- src/main/java/com/coveros/demo/helloworld/HelloWorld.java
+++ src/main/java/com/coveros/demo/helloworld/HelloWorld.java
@@ -8,7 +8,8 @@ public class HelloWorld {
   public static void main(final String[] args) {
     final DateTimeFormatter dtf = DateTimeFormatter.ofPattern("h:mm:ss a 'on' MMMM d, yyyy'.'");
     final LocalDateTime now = LocalDateTime.now();
-// sou o maoir
+// sou o maior
+//versao 2
     System.out.println("Hello, World! The current time is " + dtf.format(now));
   }
 
