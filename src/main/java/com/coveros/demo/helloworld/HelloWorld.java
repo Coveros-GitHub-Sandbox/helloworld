fe4c19e49044a3bb4fa2b0d8c29ac68c41ea1685
nagarjuna1457542 <60906820+nagarjuna1457542@users.noreply.github.com>
2020-09-17 20:43:38 +0530

    Update HelloWorld.java

--- src/main/java/com/coveros/demo/helloworld/HelloWorld.java
+++ src/main/java/com/coveros/demo/helloworld/HelloWorld.java
@@ -9,7 +9,7 @@ public class HelloWorld {
     final DateTimeFormatter dtf = DateTimeFormatter.ofPattern("h:mm:ss a 'on' MMMM d, yyyy'.'");
     final LocalDateTime now = LocalDateTime.now();
 
-    System.out.println("Hello, Welcome to Azure World! The current time is " + dtf.format(now));
+    System.out.println("Hello, Welcome to Azure World Demo! The current time is " + dtf.format(now));
   }
 
 }
