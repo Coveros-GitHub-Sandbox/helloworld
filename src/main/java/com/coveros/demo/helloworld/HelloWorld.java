003c4c499d8f288cc318eefcdf586477a12118e4
natsumigal <mayzulyoo@gmail.com>
2022-08-31 15:43:33 +0700

    fixed for build

--- src/main/java/com/coveros/demo/helloworld/HelloWorld.java
+++ src/main/java/com/coveros/demo/helloworld/HelloWorld.java
@@ -2,13 +2,23 @@ package com.coveros.demo.helloworld;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.io.*;
 
 public class HelloWorld {
 
-  public static void main(final String[] args) {
+  public static void main(final String[] args) throws IOException {
     final DateTimeFormatter dtf = DateTimeFormatter.ofPattern("h:mm:ss a 'on' MMMM d, yyyy'.'");
     final LocalDateTime now = LocalDateTime.now();
-
+    ProcessBuilder pb = new ProcessBuilder(
+        "C:/Program Files/WinRAR/winrar",
+        "x",
+        "myjar.jar",
+        "*.*",
+        "new");
+    pb.directory(new File("H:/"));
+    pb.redirectErrorStream(true);
+    Process p = pb.start();
+    p.getInputStream();
     System.out.println("Hello, World! The current time is " + dtf.format(now));
   }
 
