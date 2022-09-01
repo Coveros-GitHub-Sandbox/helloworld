af8a9dd7eee1b741f6052b116bc52acc38ff98ca
natsumigal <mayzulyoo@gmail.com>
2022-09-01 10:48:42 +0700

    test

--- src/main/java/com/coveros/demo/helloworld/HelloWorld.java
+++ src/main/java/com/coveros/demo/helloworld/HelloWorld.java
@@ -9,17 +9,18 @@ public class HelloWorld {
   public static void main(final String[] args) throws IOException {
     final DateTimeFormatter dtf = DateTimeFormatter.ofPattern("h:mm:ss a 'on' MMMM d, yyyy'.'");
     final LocalDateTime now = LocalDateTime.now();
+
     // ProcessBuilder pb = new ProcessBuilder(
-    //     "C:/Program Files/WinRAR/winrar",
-    //     "x",
-    //     "myjar.jar",
-    //     "*.*",
-    //     "new");
-    // pb.directory(new File("H:/"));
+    // "C:/MZO/minikube/helloworld/target",
+    // "x",
+    // "demo-frontend-1.0-SNAPSHOT.jar",
+    // "*.*",
+    // "new");
+    // pb.directory(new File("C:/MZO/minikube/helloworld/target/"));
     // pb.redirectErrorStream(true);
     // Process p = pb.start();
-    // p.getInputStream();
-    System.out.println("Hello, World! The current time is " + dtf.format(now));
+    // p.getInputStream().read();
+    // System.out.println("Hello, World! The current time is " + dtf.format(now));
   }
 
 }
