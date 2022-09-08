57cd7b534cf75f2a6a9bbc7e78acfb8163bb93a2
AbhishekB7245 <112380389+AbhishekB7245@users.noreply.github.com>
2022-09-08 16:02:18 +0530

    Create circletestIT.java

---
+++ src/integration-test/java/circletestIT.java
@@ -0,0 +1,13 @@
+package io.codejournal.maven.failsafedemo;
+import org.junit.jupiter.api.Tests;
+
+public class circleTestIT {
+  
+  @Test
+  public void integrationtest1(){
+  }
+  @Test
+  public void integrationtest2(){
+  }
+
+}
