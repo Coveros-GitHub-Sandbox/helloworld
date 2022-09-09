20d09d6071acdf1c3df8b95278c582b09be7f8cb
AbhishekB7245 <112380389+AbhishekB7245@users.noreply.github.com>
2022-09-09 09:09:55 +0530

    Update HelloWorld.java

--- src/main/java/com/coveros/demo/helloworld/HelloWorld.java
+++ src/main/java/com/coveros/demo/helloworld/HelloWorld.java
@@ -8,6 +8,8 @@ public class HelloWorld {
   public static void main(final String[] args) {
     final DateTimeFormatter dtf = DateTimeFormatter.ofPattern("h:mm:ss a 'on' MMMM d, yyyy'.'");
     final LocalDateTime now = LocalDateTime.now();
+    final LocalDateTime now = LocalDateTime.now()
+    final LocalDateTime now = LocalDateTime.now()
 
     System.out.println("Hello, World! The current time is " + dtf.format(now));
   }
