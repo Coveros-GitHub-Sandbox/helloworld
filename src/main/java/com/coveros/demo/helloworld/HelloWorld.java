621cf7b2de5ade169ac94ba1ca1c41b0ec56a64a
AbhishekB7245 <112380389+AbhishekB7245@users.noreply.github.com>
2022-09-09 09:12:41 +0530

    Update HelloWorld.java

--- src/main/java/com/coveros/demo/helloworld/HelloWorld.java
+++ src/main/java/com/coveros/demo/helloworld/HelloWorld.java
@@ -10,6 +10,7 @@ public class HelloWorld {
     final LocalDateTime now = LocalDateTime.now();
     final LocalDateTime now = LocalDateTime.now()
     final LocalDateTime now = LocalDateTime.now()
+    final LocalDateTime now = LocalDateTime.now()
 
     System.out.println("Hello, World! The current time is " + dtf.format(now));
   }
